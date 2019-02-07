217a8f7deb2b5bbe5d6853bc7b6ea80abb3de7a6
InfixRemix <solomonbooth63@gmail.com>
2019-02-07 22:11:54 +0000

    Code clean up for future repos

--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 now=$(date +"%T")
 echo "Script Executed at : $now"
-
+MISSING="Missing Repo was found and is now being fixed"
 if [ ! -d vendor/qcom/opensource/interfaces ]
 then
     rm -rf vendor/qcom/opensource/interfaces
     git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-interfaces vendor/qcom/opensource/interface    
-    echo " Missing Repo was found and was cloned "
+    echo $MISSING
     pwd
 fi
 
@@ -14,7 +14,7 @@ if [ ! -d vendor/qcom/opensource/dataservices ]
    then 
    rm -rf vendor/qcom/opensource/dataservices
    git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-dataservices vendor/qcom/opensource/dataservices
-   echo " Missing Repo was found and was cloned "
+   echo $MISSING
 fi
 
 # this happens even if the directory does exist
