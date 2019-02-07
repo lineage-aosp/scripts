caa1f51e98f602f1f9c077e49cb7a82d77ea7730
InfixRemix <solomonbooth63@gmail.com>
2019-02-07 22:00:46 +0000

    Add gps to main script

--- main.sh
+++ main.sh
@@ -2,21 +2,26 @@
 now=$(date +"%T")
 echo "Script Executed at : $now"
 
-if [ ! -d vendor/qcom/opensource/interfaces ]; then
+if [ ! -d vendor/qcom/opensource/interfaces ]
+then
     rm -rf vendor/qcom/opensource/interfaces
     git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-interfaces vendor/qcom/opensource/interfaces
     pwd
-
-elif
-    git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-interfaces vendor/qcom/opensource/interfaces
 fi
 
 echo " First repo done "
 
 if [ ! -d vendor/qcom/opensource/dataservices ]
    then 
-   rm -rf vendor/qcom/opensource/data-services
-   git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-dataservices
+   rm -rf vendor/qcom/opensource/dataservices
+   git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-dataservices vendor/qcom/opensource/dataservices
 fi
+
+if [ ! -d hardware/qcom/gps ]
+   then 
+   rm -rf hardware/qcom/gps
+   git clone https://github.com/sonyxperiadev/hardware-qcom-gps-sdm845 hardware/qcom/gps
+fi
+
 now=$(date +"%T")
 echo "Script Finished at : $now"
