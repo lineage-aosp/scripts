e8c9fb2092ca8ef2612c1ddbdc62efff55d2a904
InfixRemix <solomonbooth63@gmail.com>
2019-02-07 22:07:53 +0000

    Remove gps even  if it does exist & update if statements

--- main.sh
+++ main.sh
@@ -5,23 +5,22 @@ echo "Script Executed at : $now"
 if [ ! -d vendor/qcom/opensource/interfaces ]
 then
     rm -rf vendor/qcom/opensource/interfaces
-    git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-interfaces vendor/qcom/opensource/interfaces
+    git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-interfaces vendor/qcom/opensource/interface    
+    echo " Missing Repo was found and was cloned "
     pwd
 fi
-
-echo " First repo done "
 
 if [ ! -d vendor/qcom/opensource/dataservices ]
    then 
    rm -rf vendor/qcom/opensource/dataservices
    git clone https://github.com/sonyxperiadev/vendor-qcom-opensource-dataservices vendor/qcom/opensource/dataservices
+   echo " Missing Repo was found and was cloned "
 fi
 
-if [ ! -d hardware/qcom/gps ]
-   then 
-   rm -rf hardware/qcom/gps
-   git clone https://github.com/sonyxperiadev/hardware-qcom-gps-sdm845 hardware/qcom/gps
-fi
+# this happens even if the directory does exist
+rm -rf hardware/qcom/gps
+git clone https://github.com/sonyxperiadev/hardware-qcom-gps-sdm845 hardware/qcom/gps
+
 
 now=$(date +"%T")
 echo "Script Finished at : $now"
