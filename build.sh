ff20dd237d057a2ace23400b0cc81f9e29a0f18b
InfixRemix <solomonbooth63@gmail.com>
2019-03-07 21:56:11 +0000

    Import lineage build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Installing figlet"
+sudo apt-get install figlet -y
+
+figlet Welcome To InfixRemixs Script
+
+echo "What is Your Sony Device Name:"
+read device
+devicedir=device/sony/$device
+
+echo "What is rom are you building this is for .mk files so see what is required:"
+read rom
+
+cd $devicedir
+mv lineage_$device".mk" $rom"_"$device".mk"
+newmk= $rom"_"$device".mk"
+
+sed -i -e 's/lineage/'$rom/g $newmk
+sed -i -e 's/lineage/'$rom/g vendorsetup.sh
+sed -i -e 's/lineage/'$rom/g AndroidProducts.mk
+cd "$OLDPWD"
+
+. build/envsetup.sh
+lunch $device"_"$rom
+
+mka bacon
