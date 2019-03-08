27ee308a7bf69594604efd740c23a8db301ee824
InfixRemix <solomonbooth63@gmail.com>
2019-03-08 01:25:03 +0000

    make script more noob friendly

--- build.sh
+++ build.sh
@@ -4,16 +4,19 @@ sudo apt-get install figlet -y
 
 figlet Welcome To InfixRemixs Script
 
-echo "What is Your Sony Device Name:"
+echo "What is Your Sony Device model number (eg. h113):"
+read modelnumber
+
+echo "What is Your Sony Device  (eg. pioneer):"
 read device
 devicedir=device/sony/$device
 
-echo "What is the rom are you building this is for .mk files so see what is required use AOSP CODENAME:"
+echo "What is the rom are you building this is for .mk files so see what is required use AOSP MODEL NUMBER (eg cosp):"
 read rom
 
 cd $devicedir
-mv aosp_$device".mk" $rom"_"$device".mk"
-newmk= $rom"_"$device".mk"
+mv aosp_$device".mk" $rom"_"$modelnumber".mk"
+newmk= $rom"_"$modelnumber".mk"
 
 export TARGET_KERNEL_SOURCE = kernel/sony/msm-4.9/kernel
 
@@ -25,6 +28,6 @@ cd "$OLDPWD"
 bash main.sh 
 
 . build/envsetup.sh
-lunch $device"_"$rom
+lunch $modelnumber"_"$rom
 
 mka bacon
